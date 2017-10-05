43f1bad79c71b615a2f93f9d33800e1144b230cb
zingho <david.singh@lernende.bfo-vs.ch>
2017-10-05 21:21:08 +0200

    Revert "Hinzufügen Portfolio"
    This reverts commit 6d82c7fa9a5aa86c1906989ebb99df82b41c677f.

--- index.php
+++ index.php
@@ -39,63 +39,26 @@
 	<div class="text">
 		<p>Beside all the well-known places, there are hidden treasures far away from the mainstream tourism. Balance the luxury of Switzerland with local habits, accommodation and cultural events.</br>
 
-			Our focus are individuals and small groups who want to experience Switzerland away from the mainstream where you visit authentic and unique destinations and admire what Swiss pioneers have built.</br></br></br>
+			Our focus are individuals and small groups who want to experience Switzerland away from the mainstream where you visit authentic and unique destinations and admire what Swiss pioneers have built.</br></br>
 
-			<h3>You want to</h3></br>
+			<b>You want to</b></br>
 
 				<ul><li>Round up your company visit in Switzerland with an extraordinary experience</li>
 				<li>Reward your valuable employees with an unforgettable present</li>
 				<li>Grant yourself with a tailor-made and memorable involvement into the day to day life in Switzerland</li>
-				<li>All tours are tailor-made to your individual needs and preferences. We want you to enjoy your stay and to go home with unforgettable memories.</li></ul></br><br></p>
+				<li>All tours are tailor-made to your individual needs and preferences. We want you to enjoy your stay and to go home with unforgettable memories.</li></ul></br><br>
 
 			 
-				<h3>We offer</h3> </br>
-				<p>
-				<ul><li>Comprehensive service during the whole tour </li>
-				<li>Experience and Know-How in organizing tours in Switzerland</li>
-				<li>A personal English speaking guide</li>
-				<li>Your personal bus / limousine with driver</li>
-				<li>Local tour guides in Chinese and other languages</li></ul></p>
-	</div>
-	</br></br>
-	
-	<div id="artikel">
-	<h3>Our Portfolio</h3></br>
-	
-	<div class="artikel">
-	<img src="images/Destinations.jpg" class="artikel1"></br></br
-	<p class="beschreibung"><b>Destinations</b></p></br>
-	<p class="beschreibung">Well known destinations such as Zermatt, Lucerne, Interlaken, Zurich, Weggis, Bern, Schaffhausen, Montreux, Lausanne, Geneva, St.Moritz, Gstaad, Bad Ragaz ... and small villages such as Törbel and Gasenried ...</p></div>
-	
-	<div class="artikel">
-	<img src="images/Culture.jpg" class="artikel1"></br></br>
-	<p class="beschreibung"><b>Culture and art</b></p></br>
-	<p class="beschreibung">Cathedrals, Churches, Monasteries, Museums, Alphorn playing, Yodeling festival, Concerts, Wine tasting, Glass manufacturer, Swiss folklore, Swiss traditions, make your own chocolate ... </p></div>
-	<p class="clear"></p>
-	
-	<div class="artikel">
-	<img src="images/Attractions.jpg" class="artikel1"></br></br>
-	<p class="beschreibung"><b>Attractions</b></p></br>
-	<p class="beschreibung">Visit various UNESCO World Heritages, Matterhorn, Mount Pilatus, Mount Rigi, Mount Titlis, Rhine Falls, shopping in Zurich and Lucerne, Cappel bridge Lucerne, Center of Switzerland, Matterhorn Glacier Paradise, Top of Europe, Jungfrau-Joch, Château de Chillon, Aare Gorge, Swiss Museum of Transport, International Red Cross Museum …</p></div>
-	
-	<div class="artikel">
-	<img src="images/Pioneering.jpg" class="artikel1"></br></br>
-	<p class="beschreibung"><b>Pioneering spirit places and records</b></p></br>
-	<p class="beschreibung">such as the longest railway tunnel (57 km), the highest cable car, the oldest funicular railway still in use, the steepest funicular in Europe, the world first Cabrio cable car with an open upper deck, the biggest water falls in Europe, the greatest glacier of the alps, Europe’s biggest subterranean lake, one of the world largest cave systems and many more ...</p></div>
-	<p class="clear"></p>
-	
-	<div class="artikel">
-	<img src="images/Accommodations.jpg" class="artikel1"></br></br>
-	<p class="beschreibung"><b>Accommodations</b></p></br>
-	<p class="beschreibung">Full range from 5* hotels with own spa, premium hotels, Swiss Chalets, mountain lodge, fortress, igloo, a modest cottage ...</br></br></br></p></div>
+				<b>We offer</b> </br>
 
-	<div class="artikel">
-	<img src="images/Transportations.jpg" class="artikel1"></br></br>
-	<p class="beschreibung"><b>Transportations </b></p></br>
-	<p class="beschreibung">Choose between VIP limousine, helicopter, air plane, public transportation, oldtimer, Cogwheel steam railway, Glacier Express, boat, cruise ship, cable car, funicular railway, horse-drawn carriage ...</p></div>
-	
-	
+				<ul><li>Comprehensive service during the whole tour </li></br>
+				<li>Experience and Know-How in organizing tours in Switzerland</li></br>
+				<li>A personal English speaking guide</li></br>
+				<li>Your personal bus / limousine with driver</li></br>
+				<li>Local tour guides in Chinese and other languages</li></ul></p>
+  
 	</div>
+	
 	
 	<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
 
